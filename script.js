422f1ca16cb76b12e8efd87cc7f8cee2e3cce24e
Igor Gonçalves <igordossantos@gmail.com>
2021-06-28 17:06:52 -0300

    Creating food

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ snake[0] = {
     y: 8 * box
 }
 
-let derection = 'right';
+let direction = 'right';
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBg() {
     context.fillStyle = 'black'; //Estilo do canvas
@@ -22,9 +27,31 @@ function criarSnake() {
     }
 }
 
+function criarFood() {
+    context.fillStyle = 'red';
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function start() {
+
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if (snake[0].y < 0  && direction == 'up') snake[0].y = 16 * box;
+    
+
     criarBg();
     criarSnake();
+    criarFood();
 
     let snakex = snake[0].x;
     let snakey = snake[0].y;
