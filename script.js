acbda0ecb4ad266316942a2ee5e4996550751c18
Igor Gonçalves <igordossantos@gmail.com>
2021-06-28 16:38:55 -0300

    Moviment of the snake

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ snake[0] = {
     y: 8 * box
 }
 
+let derection = 'right';
 
 function criarBg() {
-    context.fillStyle = 'lightgreen'; //Estilo do canvas
+    context.fillStyle = 'black'; //Estilo do canvas
     context.fillRect(0, 0, 16 * box, 16 * box); //Desenha o retângulo x, y, altura e largura
 }
 
@@ -21,5 +22,24 @@ function criarSnake() {
     }
 }
 
-criarBg();
-criarSnake();
+function start() {
+    criarBg();
+    criarSnake();
+
+    let snakex = snake[0].x;
+    let snakey = snake[0].y;
+
+    if (direction == 'right') snakex += box;
+    if (direction == 'left') snakex -= box;
+    if (direction == 'up') snakey -= box;
+    if (direction == 'down') snakey += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakex,
+        y: snakey
+    }
+    snake.unshift(newHead);
+}
+let game = setInterval(start, 100);
